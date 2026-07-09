0ec6fa3b503008253b6022dba4de69cc1719f9e2
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-09 16:03:20 +0700

    feat: menambahkan migrasi tabel produk

---
+++ database/migrations/2026_07_09_084215_create_produk_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->id('id_produk');
+            $table->string('nama_produk', 255);
+            $table->unsignedBigInteger('id_kategori');
+            $table->unsignedBigInteger('id_satuan');
+            $table->decimal('harga_beli', 12, 2);
+            $table->decimal('harga_jual', 12, 2);
+            $table->integer('stok')->default(0);
+            $table->integer('reorder_point')->default(10);
+            $table->foreign('id_kategori')->references('id_kategori')->on('kategori_produk');
+            $table->foreign('id_satuan')->references('id_satuan')->on('satuan_ukur');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produk');
+    }
+};
