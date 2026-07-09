af25328415f63309acfe464e7000b9e34d8888f6
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-09 16:03:54 +0700

    feat: menambahkan migrasi tabel mutasi

---
+++ database/migrations/2026_07_09_084221_create_mutasi_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mutasi', function (Blueprint $table) {
+            $table->id('id_mutasi');
+            $table->date('tanggal');
+            $table->string('nama_produk', 255);
+            $table->string('jenis_mutasi', 20);
+            $table->integer('jumlah');
+            $table->text('keterangan')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mutasi');
+    }
+};
