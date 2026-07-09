f3a7123cc44e26d0e155ce6f8b9e457d1942d5dc
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-09 16:00:38 +0700

    feat: menambahkan satuan controller

---
+++ app/Http/Controllers/SatuanController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SatuanUkur;
+use Illuminate\Http\Request;
+
+class SatuanController extends Controller
+{
+    public function index()
+    {
+        $satuan = SatuanUkur::all();
+        return view('satuan.index', compact('satuan'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
