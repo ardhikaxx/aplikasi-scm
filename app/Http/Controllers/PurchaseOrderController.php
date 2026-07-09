1d67693b0016707005eeca2983b5f8b4b6ad04dc
ardhikaxx <ardhikayanuar58@gmail.com>
2026-07-09 16:00:50 +0700

    feat: menambahkan purchase order controller

---
+++ app/Http/Controllers/PurchaseOrderController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PurchaseOrder;
+use Illuminate\Http\Request;
+
+class PurchaseOrderController extends Controller
+{
+    public function index()
+    {
+        $purchaseOrders = PurchaseOrder::with('supplier')->get();
+        return view('purchase_order.index', compact('purchaseOrders'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
